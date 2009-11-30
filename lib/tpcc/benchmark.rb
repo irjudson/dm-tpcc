52d430947a18fa30a84767ecbaa2afd3bd21e0c9
irjudson <irjudson@gmail.com>
2009-11-30 14:10:24 -0700

    Updating to working code!

--- lib/tpcc/benchmark.rb
+++ lib/tpcc/benchmark.rb
@@ -1,7 +1,8 @@
 module DataMapper
  module TPCC
    def self.benchmark
-    puts "Benchmarks not implemented yet."
+      puts "Benchmarks not implemented yet."
+      bm = Benchmark.new
    end
    
    class Benchmark
@@ -10,7 +11,35 @@ module DataMapper
      # as methods and has helper methods to capturing timing information and
      # format it for output.
      #
+     attr_accessor :transaction
      
+     def initialize
+       self.transaction = DataMapper::Transaction.new(repository(:default))
+     end
+     
+     def self.new_order_transaction
+       raise NotImplementedError
+     end
+     
+     def self.payment_transaction
+       raise NotImplementedError
+     end
+     
+     def order_status_transaction
+       raise NotImplementedError
+     end
+     
+     def delivery_transaction
+       raise NotImplementedError
+     end
+     
+     def stock_level_transaction
+       # Select a threshold randomly between 10 and 20
+       # For a given warehouse and a given district
+       # take the all items for the last 20 orders
+       # for each item check to see if the # of items in stock is < threshold 
+       # return the number of items that have quantities below threshold
+     end
    end
  end
 end
--- lib/tpcc/load.rb
+++ lib/tpcc/load.rb
@@ -7,57 +7,99 @@ module DataMapper
     #
     def self.load(num_warehouses = 1)
 
-      GC.start()
+      scale = {
+        :warehouse => num_warehouses,
+        :district => num_warehouses * 10,
+        :customer => num_warehouses * 30000,
+        :order => num_warehouses * 30000,
+        :history => num_warehouses * 30000,
+        :new_order => num_warehouses * 9000,
+        :order_line => num_warehouses * 300000,
+        :stock => num_warehouses * 100000,
+        :item => 100000
+      }
+
+      total_time = ::Benchmark.realtime {
+        make_history(scale[:history])
       
-      make_history(num_warehouses)
+        make_new_orders(scale[:new_order])
+    
+        make_order_lines(scale[:order_line])
+
+        make_warehouse_tree(scale[:warehouses])
       
-      GC.start()
-      
-      duration = ::Benchmark.realtime { 
-        DataMapper::Repository.adapters[:default].execute("lock tables new_orders write;")
-        (num_warehouses*9000).of { NewOrder.gen } 
-        DataMapper::Repository.adapters[:default].execute("unlock tables;")            
+        make_items(scale[:item])
       }
       
-      puts "Created #{num_warehouses*9000} NewOrder instances in #{"%.3f" % duration} seconds."
-
-      GC.start()
-      
-      return
-
-      duration = ::Benchmark.realtime {
-        DataMapper::Repository.adapters[:default].execute("lock tables order_lines write;")
-        3.times {
-          (num_warehouses*100000).of { OrderLine.gen }
-        }
-        DataMapper::Repository.adapters[:default].execute("unlock tables;")                    
-      }
-      puts "Created #{num_warehouses*100000} OrderLine instances in #{"%.3f" % duration} seconds."        
-
-      duration = ::Benchmark.realtime { 
-        DataMapper::Repository.adapters[:default].execute("lock tables customers write;")
-        DataMapper::Repository.adapters[:default].execute("lock tables stocks write;")
-        DataMapper::Repository.adapters[:default].execute("lock tables orders write;")
-        num_warehouses.of { Warehouse.gen } 
-        DataMapper::Repository.adapters[:default].execute("unlock tables;")                    
-      }
-      puts "Created #{num_warehouses} Warehouse instances in #{"%.3f" % duration} seconds."
-
-      duration = ::Benchmark.realtime { 
-        DataMapper::Repository.adapters[:default].execute("lock tables items write;")
-        100000.of { Item.gen } 
-        DataMapper::Repository.adapters[:default].execute("unlock tables;")                    
-      }
-      puts "Created #{100000} Item instances in #{"%.3f" % duration} seconds."
+      puts "Created Initial Dataset in #{total_time} seconds."
     end
     
-    def self.make_history(num_warehouses)
-      duration = ::Benchmark.realtime { 
-          DataMapper::Repository.adapters[:default].execute("lock tables histories write;")
-          (num_warehouses*30000).of { History.gen } 
-          DataMapper::Repository.adapters[:default].execute("unlock tables;")      
-      }
-      puts "Created #{num_warehouses*30000} History instances in #{"%.3f" % duration} seconds."
+    def self.make_thing(number, klass)
+      duration = ::Benchmark.realtime { self.transaction.commit { number.time { klass.gen } } }
+      puts "Created #{number} #{klass.name} instances in #{"%.3f" % duration} seconds."
+    end
+    
+    def self.make_history(number)
+      transaction = DataMapper::Transaction.new(repository(:default))
+      duration = ::Benchmark.realtime { transaction.commit { number.times { History.gen } } }
+      puts "Created #{number} History instances in #{"%.3f" % duration} seconds."
+    end
+    
+    def self.make_new_orders(number)
+      transaction = DataMapper::Transaction.new(repository(:default))
+      duration = ::Benchmark.realtime { transaction.commit { number.times { NewOrder.gen } } }      
+      puts "Created #{number} NewOrder instances in #{"%.3f" % duration} seconds."
+    end
+    
+    def self.make_order_lines(number)
+      transaction = DataMapper::Transaction.new(repository(:default))
+      duration = ::Benchmark.realtime {transaction.commit { number.times { OrderLine.gen } } }
+      puts "Created #{number} OrderLine instances in #{"%.3f" % duration} seconds."        
+    end
+    
+    def self.make_warehouse_tree(number)
+      transaction = DataMapper::Transaction.new(repository(:default))
+      duration = ::Benchmark.realtime { transaction.commit { number.times { Warehouse.gen } } }
+      puts "Created #{number} Warehouse instances in #{"%.3f" % duration} seconds."
+    end
+    
+    def self.make_items(number)
+      transaction = DataMapper::Transaction.new(repository(:default))
+      duration = ::Benchmark.realtime { transaction.commit { number.times { Item.gen } } }
+      puts "Created #{number} Item instances in #{"%.3f" % duration} seconds."
     end
   end
+
+  # DM-Sweatshop is evil and tries to keep a reference to every object allocated.
+  class Sweatshop
+    class << self
+      attr_accessor :object_count
+      attr_accessor :object_threshold
+    end
+    
+     self.record_map = Hash.new {|h,k| h[k] = Hash.new {|h,k| h[k] = 0}}
+     self.object_count = 0
+     self.object_threshold = 25000
+     
+     def self.record(klass, name, instance)
+       # self.record_map[klass][name.to_sym] << instance
+       self.record_map[klass][name.to_sym] += 1
+
+       self.object_count += 1
+       if self.object_count > self.object_threshold
+         GC.start
+         self.object_count = 0
+       end
+       
+       instance
+     end
+
+     def self.pick(klass, name)
+       # self.record_map[klass][name.to_sym].pick || raise(NoFixtureExist, "no #{name} context fixtures have been generated for the #{klass} class")
+       offset = (rand * self.record_map[klass][name.to_sym]).to_i + 1
+       klass.first(:offset => offset) || raise(NoFixtureExist, "no #{name} context fixtures have been generated for the #{klass} class")
+     end
+
+   end
 end
+
